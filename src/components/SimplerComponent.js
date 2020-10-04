e53ec98a5e342235cffe3879d02388f33dedfb0f
Chance Smith <chancesmith97@hotmail.com>
2020-10-04 20:47:50 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,12 @@
 // Code SimplerComponent Here
+import React, { Component } from 'react';
+
+export default class SimplerComponent extends Component {
+
+
+  render() {
+    return (
+
+    )
+  }
+}
